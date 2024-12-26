db3596a5571a47ccb6395f5b816c1a12e02aadca
Shahbaz Alam <sbzalam2024@gmail.com>
2024-12-27 00:42:00 +0530

    some modified

--- pythons/BankProject/acconts.py
+++ pythons/BankProject/acconts.py
@@ -26,38 +26,32 @@ class Account:
         return self.balance
            
 
-       
-# ac=Account(100)
-# ac.credit(100)
-# ac.debit(50)
-# ac.get_balance()
-
-    
+         
       
         
 class Sevingsaccount(Account):
     
-    def __init__(self,iterest_rate=0.0,balance=0.0):
-        self.iterest_rate=iterest_rate
+    def __init__(self,interest_rate=0.0,balance=0.0):
+        self.interest_rate=interest_rate
         super().__init__(balance)
       
         
     def calculate_interest(self):
-        interest_earned = self.balance * self.iterest_rate
+        interest_earned = self.balance * self.interest_rate
         self.balance += interest_earned 
-        print("initial balance after interest: ",self.balance)
+        # print("initial balance after interest: ",self.balance)
         return self.balance 
         
     
     def credit(self,amount):
         self.balance+=amount
-        print("initial balance after credit: ",self.balance)
+        # print("initial balance after credit: ",self.balance)
         
         
     def debit(self,amount):
         if amount<=self.balance:
             self.balance-=amount
-            print("initial balance after debit: ",self.balance)
+            # print("initial balance after debit: ",self.balance)
            
         else:
             print("Debit amount exceeded account balance.")
@@ -76,14 +70,14 @@ class Checkingaccount(Account):
     def credit(self,amount):
         self.balance+=amount
         self.balance-=self.fee_charge
-        print("initial balance after credit and fee charge : ",self.balance)
+        # print("initial balance after credit and fee charge : ",self.balance)
         
         
     def debit(self,amount):
         if amount<=self.balance:
             self.balance-=amount
             self.balance-=self.fee_charge
-            print("initial balance after debit and fee charge: ",self.balance)
+            # print("initial balance after debit and fee charge: ",self.balance)
            
         else:
             print("Debit amount exceeded account balance.")
@@ -104,23 +98,23 @@ class Currentaccount(Account):
         
     def credit(self,amount):
         self.balance+=amount
-        print("initial balance after credit : ",self.balance)
+        # print("initial balance after credit : ",self.balance)
     
         
     def debit(self,amount):
         if self.balance>= amount:
             self.balance-=amount
-            print("initial balance (greater than amount) after debit : ",self.balance)
+            # print("initial balance (greater than amount) after debit : ",self.balance)
         elif self.balance< amount and self.balance>0:
             if (amount-self.balance) <= self.overdraft_limit:
                 self.balance -= amount
-                print("initial balance (lesser than amount) after debit : ",self.balance)
+                # print("initial balance (lesser than amount) after debit : ",self.balance)
             else:
                 print("Debit amount exceeded to Overdraft limit.")
         elif self.balance<=0:
             if amount< self.overdraft_limit:
                 self.balance-=amount
-                print("initial balance (lesser than 0) after debit : ",self.balance)   
+                # print("initial balance (lesser than 0) after debit : ",self.balance)   
             else:
                 print("Debit amount exceeded to Overdraft limit.")
 
